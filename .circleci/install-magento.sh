5e1ff1d3fb75cbd7b610648ee5e4afa679ae7a99
Christophe Jelger <jelger@adobe.com>
2020-08-12 10:51:53 +0200

    CIF-1499 - Setup IT infrastructure for Venia Demo
    - test install with docker-compose

---
+++ .circleci/install-magento.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+docker-compose up -d
+
+# Wait for Magento to be installed
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost/graphql)" != "200" ]];
+do echo "Waiting for Magento installation to complete ..." && sleep 15;
+done
+
+# Get Magento container id
+CONTAINER=`docker ps | grep "bitnami/magento" | cut -f1 -d " "`
+
+# Copy Magento repo credentials and install sample data
+echo $MAGENTO_AUTH_JSON > auth.json
+docker cp auth.json $CONTAINER:/opt/bitnami/magento/htdocs/auth.json
+docker exec -it $CONTAINER php /opt/bitnami/magento/htdocs/bin/magento sampledata:deploy
+docker exec -it $CONTAINER php /opt/bitnami/magento/htdocs/bin/magento setup:upgrade
